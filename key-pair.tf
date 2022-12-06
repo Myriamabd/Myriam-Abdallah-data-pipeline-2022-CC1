c6909da827cf33d55206117cf8d5eab5b9e4dccd
Myriamabd <76534861+Myriamabd@users.noreply.github.com>
2022-12-06 17:49:21 +0100

    Delete key-pair.tf

--- key-pair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "myriam-abdallah-key" {
-  key_name   = "myriam-abdallah-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtuxaauJj+WTxehkaq/olAF0zxYbnoGSzCUiyUpdiM45EeSxz3BqBufMRdC8j+WoY4JPlXZsfX7SCSxlVczXtigBBqVgs03W5R+woXxW7cK3/wOc5AohBLqV4UwDr5E6vpYSA9f+KPBI0kk/XLo10+QKXJ/PW5lQbj2Ij30tHjusEW0QscJGNIfnfLsw4Kwlx9Zvxo1nf50qQk0dO1QQQT7/bfwOLZf/klwhwRMwbWs7hUwWbTJdqGthHlcGnBYvQBd98fy749zYkDN83j5tA/CnZI7+pky1qaJXP7ZhcPw6O4DYJkzRrOv2xGh+0dFAbp7yWQ/NzgcBajcEQFGqGH"
-}
